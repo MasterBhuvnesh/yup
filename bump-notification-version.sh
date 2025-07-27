82dd0651573a8a4993e124e7af75d6efb77a3656
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2025-07-27 11:38:11 +0530

    test : fiximg (#1)

---
+++ bump-notification-version.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# Default to "patch" if no argument is provided
+VERSION_TYPE=${1:-patch}
+
+# Validate version type
+if [[ "$VERSION_TYPE" != "patch" && "$VERSION_TYPE" != "minor" && "$VERSION_TYPE" != "major" ]]; then
+  echo "Error: Invalid version type. Use one of: patch, minor, major."
+  exit 1
+fi
+
+# Step into the notification folder and bump version
+cd notification
+npm version "$VERSION_TYPE"
+
+# Go back to root and commit the changes
+cd ..
+git add notification/package.json notification/package-lock.json
+git commit -m "chore(notification): bump $VERSION_TYPE version"
+git push origin main
+
+# Push version tag
+VERSION=$(node -p "require('./notification/package.json').version")
+git push origin "v$VERSION"
